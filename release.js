01a8a9cc11427a871447c1becfdf7ece1d253a68
jeantimex <jean.timex@gmail.com>
2017-08-17 12:33:41 -0700

    Added check.

--- release.js
+++ release.js
@@ -22,32 +22,34 @@ inquirer
     },
   ])
   .then(function(answer) {
-    var execOptions = { cwd: path.join(__dirname, '..') };
+    // Step 0. Double check and make sure everything looks good
+    console.log('Validating...');
+    execSync('npm run test');
+
     // Step 1. Bump up version number
     console.log('Bumping up version number...');
-    execSync('npm version ' + answer.semver, execOptions);
-    var version = require(path.join(__dirname, '..', 'package.json')).version;
+    execSync('npm version ' + answer.semver);
+    var version = require('./package.json').version;
     console.log('New version is: ' + version);
 
     // Step 2. Cut a new build
     console.log('Building new release bundle...');
-    execSync('npm run build', execOptions);
+    execSync('npm run build');
     console.log('New release bundle is built!');
 
     // Step 3. Git push the changes for package.json and release bundle
     console.log('Push new changes...');
     var message = 'Release ' + version;
-    execSync('git commit -am "' + message + '"', execOptions);
-    execSync('git push', execOptions);
+    execSync('git commit -am "' + message + '"');
+    execSync('git push');
 
     // Step 4. Tag the new version
     console.log('Tag new version ' + version);
-    execSync('git tag ' + version, execOptions);
-    execSync('git push origin ' + version, execOptions);
+    execSync('git tag ' + version);
+    execSync('git push --follow-tags --no-verify');
     console.log('Publishing to npm...');
 
     // Step 5. NPM publish
-    execSync('npm login', execOptions);
-    execSync('npm publish', execOptions);
+    execSync('npm publish');
     console.log('Done');
   });
