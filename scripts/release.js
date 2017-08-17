c10822f33a91bc5e05c992e0e71f0e569f926a97
jeantimex <jean.timex@gmail.com>
2017-08-17 12:07:44 -0700

    Updated release script.

--- scripts/release.js
+++ scripts/release.js
@@ -24,14 +24,29 @@ inquirer
   .then(function(answer) {
     var execOptions = { cwd: path.join(__dirname, '..') };
     // Step 1. Bump up version number
+    console.log('Bumping up version number...');
     execSync('npm version ' + answer.semver, execOptions);
+    var version = require(path.join(__dirname, '..', 'package.json')).version;
+    console.log('New version is: ' + version);
+
     // Step 2. Cut a new build
+    console.log('Building new release bundle...');
     execSync('npm run build', execOptions);
-    // Step 3. Git push
-    var pkg = require(path.join(__dirname, '..', 'package.json'));
-    var message = 'Release ' + pkg.version;
+    console.log('New release bundle is built!');
+
+    // Step 3. Git push the changes for package.json and release bundle
+    console.log('Push new changes...');
+    var message = 'Release ' + version;
     execSync('git commit -am "' + message + '"');
     execSync('git push');
-    // Step 4. NPM publish
+
+    // Step 4. Tag the new version
+    console.log('Tag new version ' + version);
+    execSync('git tag ' + version);
+    execSync('git push origin ' + version);
+    console.log('Publishing to npm...');
+
+    // Step 5. NPM publish
     execSync('npm publish');
+    console.log('Done');
   });
