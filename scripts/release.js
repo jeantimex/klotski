5bb6f96223f75a852388f12354d71b1cda19b9f1
jeantimex <jean.timex@gmail.com>
2017-08-17 11:47:02 -0700

    Added release script

---
+++ scripts/release.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+/**
+ * Release Scripts
+ */
+
+'use strict';
+var execSync = require('child_process').execSync;
+var path = require('path');
+var inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'semver',
+      message: 'How do you want to release?',
+      choices: ['Patch', 'Minor', 'Major'],
+      filter: function(val) {
+        return val.toLowerCase();
+      },
+    },
+  ])
+  .then(function(answer) {
+    var execOptions = { cwd: path.join(__dirname, '..') };
+    // Step 1. Bump up version number
+    execSync('npm version ' + answer.semver, execOptions);
+    // Step 2. Cut a new build
+    execSync('npm run build', execOptions);
+    // Step 3. Git push
+    // Step 4. NPM publish
+    var semver = answer.semver;
+  });
