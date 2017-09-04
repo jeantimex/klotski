9a150921a48b05a020aa2d37ef247d9f11dd4992
jeantimex <jean.timex@gmail.com>
2017-09-04 15:14:21 -0700

    Changed hero to block.

--- src/demo.js
+++ src/demo.js
@@ -2,6 +2,6 @@ var Klotski = require('./klotski');
 var games = require('./games.json');
 
 var klotski = new Klotski();
-var result = klotski.solve(games[0].heroes);
+var result = klotski.solve(games[0].blocks);
 
 console.log(result);
--- test/Klotski-test.js
+++ test/Klotski-test.js
@@ -10,7 +10,52 @@ describe('Klotski', function() {
   });
 
   it('should solve the problem', function() {
-    var result = klotski.solve(games[0].heroes);
+    var result = klotski.solve(games[0].blocks);
+    assert.equal(result[0].step, 81);
     assert.equal(result.length, 118);
+  });
+
+  it('should return null when block type is unkonwn', function() {
+    var blocks = [
+      {
+        type: 5,
+        position: [0, 1],
+      },
+    ];
+    var result = klotski.solve(blocks);
+    assert.equal(result, null);
+  });
+
+  it('should support tuple input', function() {
+    var blocks = [4, 0, 1, 2, 0, 0, 2, 0, 3, 2, 2, 0, 2, 2, 3, 3, 2, 1, 1, 3, 1, 1, 3, 2, 1, 4, 0, 1, 4, 3];
+    var result = klotski.solve(blocks);
+    assert.equal(result[0].step, 81);
+    assert.equal(result.length, 118);
+  });
+
+  it('should return null when tuple is invalid', function() {
+    var blocks = [4, 0, 1, 2, 0, 0, 2, 0, 3, 2, 2, 0, 2, 2, 3, 3, 2, 1, 1, 3, 1, 1, 3, 2, 1, 4, 0, 1, 4];
+    var result = klotski.solve(blocks);
+    assert.equal(result, null);
+  });
+
+  it('should return null when input is invalid', function() {
+    var result = klotski.solve(null);
+    assert.equal(result, null);
+  });
+
+  it('should return null when input is empty', function() {
+    var result = klotski.solve([]);
+    assert.equal(result, null);
+  });
+
+  it('should return null when input contains invalid data', function() {
+    var result = klotski.solve(['test']);
+    assert.equal(result, null);
+  });
+
+  it('should not solve the game', function() {
+    var result = klotski.solve(games[33].blocks);
+    assert.equal(result, null);
   });
 });
