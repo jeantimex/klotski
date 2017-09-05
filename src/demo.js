80a5d14211374b4ad2dabb232aee3184f44f7bfb
jeantimex <jean.timex@gmail.com>
2017-09-04 22:07:40 -0700

    Added options.

--- src/demo.js
+++ src/demo.js
@@ -2,6 +2,8 @@ var Klotski = require('./klotski');
 var games = require('./games.json');
 
 var klotski = new Klotski();
-var result = klotski.solve(games[0].blocks);
+var result = klotski.solve(games[0].blocks, {
+  useMirror: true,
+});
 
 console.log(result);
