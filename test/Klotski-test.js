f0e5e04e3386ee7811f5299b6840b28399f273f5
jeantimex <jean.timex@gmail.com>
2019-08-25 07:46:22 -0700

    Support shape instead of type.

--- test/Klotski-test.js
+++ test/Klotski-test.js
@@ -23,30 +23,6 @@ describe('Klotski', function() {
     assert.equal(result.length, 118);
   });
 
-  it('should return null when block type is unkonwn', function() {
-    var blocks = [
-      {
-        type: 5,
-        position: [0, 1],
-      },
-    ];
-    var result = klotski.solve(blocks);
-    assert.equal(result, null);
-  });
-
-  it('should support tuple input', function() {
-    var blocks = [4, 0, 1, 2, 0, 0, 2, 0, 3, 2, 2, 0, 2, 2, 3, 3, 2, 1, 1, 3, 1, 1, 3, 2, 1, 4, 0, 1, 4, 3];
-    var result = klotski.solve(blocks);
-    assert.equal(result[0].step, 81);
-    assert.equal(result.length, 118);
-  });
-
-  it('should return null when tuple is invalid', function() {
-    var blocks = [4, 0, 1, 2, 0, 0, 2, 0, 3, 2, 2, 0, 2, 2, 3, 3, 2, 1, 1, 3, 1, 1, 3, 2, 1, 4, 0, 1, 4];
-    var result = klotski.solve(blocks);
-    assert.equal(result, null);
-  });
-
   it('should return null when input is invalid', function() {
     var result = klotski.solve(null);
     assert.equal(result, null);
