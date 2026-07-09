02c6f5dde771d04f40b8c3d4516344197d228629
Yong Su <jean.timex@gmail.com>
2026-07-08 22:46:37 -0700

    clean up the demo

--- src/demo.js
+++
@@ -1,8 +0,0 @@
-var Klotski = require('./klotski');
-var hrdGames = require('./hrd-games.json');
-var customGames = require('./custom-games.json');
-
-var klotski = new Klotski();
-var result = klotski.solve(customGames[0]);
-
-console.log(result);
